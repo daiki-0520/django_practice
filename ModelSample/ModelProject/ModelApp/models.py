bfa8891efee1b23a141d66d4fe17302b8ab25bc8
daiki <daiki.20000727@gmail.com>
2021-02-27 17:39:18 +0900

    practice models1

---
+++ ModelSample/ModelProject/ModelApp/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.utils import timezone
+
+# Create your models here.
+class Person(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    birthday = models.DateField(default='1900-01-01')
+    email = models.EmailField(db_index=True, null=True)
+    salary= models.FloatField(null=True)
+    memo = models.TextField(null=True)
+    web_site = models.URLField(null=True, blank=True)
+    create_at = models.DateTimeField(default=timezone.datetime.now)
+
