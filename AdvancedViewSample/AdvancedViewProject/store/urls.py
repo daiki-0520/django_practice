01a35aeb7a5056c3af7dd846a64df3c611d42b46
daiki <daiki.20000727@gmail.com>
2021-03-08 13:15:35 +0900

    practiced error handling and redirect

---
+++ AdvancedViewSample/AdvancedViewProject/store/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+from store import views
+
+app_name = 'store'
+
+urlpatterns = [
+    path('item_list/', views.item_list, name = 'item_list'),
+    path('item_detail/<int:id>', views.item_detail, name = 'item_detail'),   
+    path('to_google/', views.to_google, name = 'to_google'),
+    path('one_item/', views.one_item, name = 'one_item'),
+]
+
