2037d13a182f150143d4d8bc1b99a14f3f6095df
daiki <daiki.20000727@gmail.com>
2021-03-17 22:17:27 +0900

    add classbasedview

---
+++ ClassBasedView/class_based_view/store/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render
+from django.views.generic.base import(
+    View, TemplateView
+)
+from . import forms
+from datetime import datetime
+# Create your views here.
+
+class IndexView(View):
+
+    def get(self, request, *args, **kwargs):
+        book_form = forms.BookForm()
+        return render(request, 'index.html', context = {
+            'book_form': book_form
+        })
+
+    def post(self, request, *args, **kwargs):
+        book_form = forms.BookForm(request.POST or None)
+        if book_form.is_valid():
+            book_form.save()
+
+        return render(request, 'index.html', context={
+            'book_form': book_form,
+        })
+
+class HomeView(TemplateView):
+    template_name = 'home.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['time'] = datetime.now()
+        return context
+
